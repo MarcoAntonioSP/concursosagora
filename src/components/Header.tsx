ab0fc2a7a239cbd3c46a47d14ec26f3c5ee4b1ab
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2024-11-21 00:21:38 -0300

    mcwerwerfssfsvv

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -79,7 +79,7 @@ export function Header() {
             <ul className="flex mr-5 font-semibold flex-col  p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
-                  href="#"
+                  href="/"
                   className="block font-semibold py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 >
                   Inicio
