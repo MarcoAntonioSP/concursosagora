fa30991a24c64f477d9cb01bcc260cd5fb0cb4ec
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2024-12-05 06:06:07 -0300

    googleAnalytcsOKandcookiesOK

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,22 +12,54 @@ const poppins = Poppins({
   subsets: ['latin'],
   variable: '--font-poppins',
 });
+
 export default function App({ Component, pageProps }: AppProps) {
   const [consentGiven, setConsentGiven] = useState(false);
+
   useEffect(() => {
     const consentStatus = localStorage.getItem('cookie-consent');
     if (consentStatus === 'true') {
       setConsentGiven(true);
+      consentGrantedAdStorage();
+    } else {
+      // Default consent state when not yet given
+      setDefaultConsentState();
     }
   }, []);
+
   const handleConsent = (consent: boolean) => {
     localStorage.setItem('cookie-consent', String(consent));
     setConsentGiven(consent);
+    if (consent) {
+      consentGrantedAdStorage();
+    }
   };
+
+  function consentGrantedAdStorage() {
+    window.gtag('consent', 'update', {
+      'ad_storage': 'granted',
+      'analytics_storage': 'granted',
+      'ad_user_data': 'granted',
+      'ad_personalization': 'granted'
+    });
+  }
+
+  function setDefaultConsentState() {
+    window.gtag('consent', 'default', {
+      'ad_storage': 'denied',
+      'analytics_storage': 'denied',
+      'ad_user_data': 'denied',
+      'ad_personalization': 'denied',
+      'wait_for_update': 500, // Waits 500ms for CMP to update consent
+      'region': ['ES', 'US-AK'] // Spain and Alaska
+    });
+  }
+
   return (
     <ApolloProvider client={client}>
       <main className={`${poppins.variable} font-sans`}>
         <SpeedInsights />
+        
         {consentGiven && (
           <>
             <script
@@ -46,6 +78,7 @@ export default function App({ Component, pageProps }: AppProps) {
             />
           </>
         )}
+
         <Component {...pageProps} />
         {!consentGiven && <CookieBanner onConsent={handleConsent} />}
       </main>
